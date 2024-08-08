1ba9634ccde7da685cc6fdb06f961b18022f7dba
omirbakit <omersat@mail.ru>
2024-08-08 23:53:54 +0500

    change to push

--- 8kyu/double-char.js
+++ 8kyu/double-char.js
@@ -1,6 +1,5 @@
 // Description:
 // Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
-
 // Examples (Input -> Output):
 // * "String"      -> "SSttrriinngg"
 // * "Hello World" -> "HHeelllloo  WWoorrlldd"
